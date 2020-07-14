799e68ba06ba54ee42c730213b8bdd983a9ac0fd
Axel Navarro <navarroaxel@gmail.com>
2020-07-14 15:17:11 -0300

    Add a page to my KDE contributions

--- notepad/index.js
+++ notepad/index.js
@@ -1,6 +1,7 @@
+/* eslint-disable */
 const setCursorPosition = () => {
-    const lines = t.value.substring(0, t.selectionEnd).split('\n');
-    l.textContent = `${lines.length}:${lines[lines.length - 1].length}`
+  const lines = t.value.substring(0, t.selectionEnd).split('\n');
+  l.textContent = `${lines.length}:${lines[lines.length - 1].length}`
 };
 t.addEventListener('keyup', setCursorPosition);
 t.addEventListener('mousedown', setCursorPosition);
